e402c580493703ef932f5ad6aa0edeed636320e0
Rahul Nakum <nakumrahul5@gmail.com>
2025-02-16 23:04:45 +0530

    created folder structure and created navbar, post compoenet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CreatePostComponent } from './features/post/pages/create-post/create-post.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   {
+    path: 'create-post',
+    component:CreatePostComponent
+  }
+];
